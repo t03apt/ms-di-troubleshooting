f8763fd5e274b658ccb85f5eb32a247bab05e0a3
t03apt <t03apt@hotmail.com>
2018-01-20 02:27:30 +0100

    add(configure service collection)

--- AspNetDI/MicrosoftDITest.cs
+++ AspNetDI/MicrosoftDITest.cs
@@ -1,75 +1,112 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AspNetDI
 {
     [TestClass]
-    public class MicrosoftDITest
+    public partial class MicrosoftDITest
     {
-
         [TestMethod]
         public void CanResolveINotificationHandlerOfINotification()
         {
             ServiceProvider provider = BuildServiceProvider();
-            GetRequiredServices(provider, typeof(INotificationHandler<INotification>));
+            var handlers = GetRequiredServices<INotificationHandler<INotification>>(provider);
+            var message = new Pinged();
+            Publish(handlers, message);
         }
 
         [TestMethod]
         public void CanResolveINotificationHandlerOfPinged()
         {
             ServiceProvider provider = BuildServiceProvider();
-            GetRequiredServices(provider, typeof(INotificationHandler<Pinged>));
+            var handlers = GetRequiredServices<INotificationHandler<Pinged>>(provider);
+            var message = new Pinged();
+            Publish(handlers, message);
         }
 
         [TestMethod]
         public void CanResolveINotificationHandlerOfPonged()
         {
             ServiceProvider provider = BuildServiceProvider();
-            GetRequiredServices(provider, typeof(INotificationHandler<Ponged>));
+            var handlers = GetRequiredServices<INotificationHandler<Ponged>>(provider);
+            var message = new Ponged();
+            Publish(handlers, message);
+        }
+
+        [TestMethod]
+        public void CanResolveINotificationHandlerOfSpecialPinged()
+        {
+            ServiceProvider provider = BuildServiceProvider();
+            var handlers = GetRequiredServices<INotificationHandler<SpecialPinged>>(provider);
+            var message = new SpecialPinged();
+            Publish(handlers, message);
+        }
+
+        private static void Publish<T>(IEnumerable<INotificationHandler<T>> handlers, T message) where T: INotification
+        {
+            handlers.ToList().ForEach(o => o.Handle(message));
+        }
+
+        private void TraceHandlers(IEnumerable<object> handlers)
+        {
+            Console.WriteLine(string.Join(" ", handlers));
         }
 
         private static ServiceProvider BuildServiceProvider()
         {
             var services = new ServiceCollection();
-
-            services.AddScoped(typeof(INotificationHandler<>), typeof(ConstrainedPingedHandler<>));
+            services.RegisterNotificationHandlers(Assembly.GetExecutingAssembly());
             var provider = services.BuildServiceProvider();
             return provider;
         }
 
-        private static IEnumerable<object> GetRequiredServices(IServiceProvider provider, Type serviceType)
+        private static IEnumerable<T> GetRequiredServices<T>(IServiceProvider provider)
         {
-            return (IEnumerable<object>)provider.GetRequiredService(typeof(IEnumerable<>).MakeGenericType(serviceType));
+            return (IEnumerable<T>)provider.GetRequiredService(typeof(IEnumerable<T>));
         }
 
-        interface INotification { }
+        public interface INotification { }
 
-        interface INotificationHandler<in TNotification>
+        public interface INotificationHandler<in TNotification>
             where TNotification : INotification
         {
             void Handle(TNotification notification);
         }
 
-        class Pinged : INotification { }
+        public class Pinged : INotification { }
 
-        class Ponged : INotification { }
+        public class Ponged : INotification { }
 
-        class PingedHandler : INotificationHandler<Pinged>
+        public class SpecialPinged : Pinged { }
+
+        public class GenericHandler : INotificationHandler<INotification>
         {
-            public void Handle(Pinged notification) { }
+            public void Handle(INotification notification) { Console.WriteLine(nameof(GenericHandler)); }
         }
 
-        class PongedHandler : INotificationHandler<Ponged>
+        public class PingedHandler : INotificationHandler<Pinged>
         {
-            public void Handle(Ponged notification) { }
+            public void Handle(Pinged notification) { Console.WriteLine(nameof(PingedHandler)); }
         }
 
-        class ConstrainedPingedHandler<TNotification> : INotificationHandler<TNotification>
+        public class SpecialPingedHandler : INotificationHandler<SpecialPinged>
+        {
+            public void Handle(SpecialPinged notification) { Console.WriteLine(nameof(SpecialPingedHandler)); }
+        }
+
+        public class PongedHandler : INotificationHandler<Ponged>
+        {
+            public void Handle(Ponged notification) { Console.WriteLine(nameof(PongedHandler)); }
+        }
+
+        public class ConstrainedPingedHandler<TNotification> : INotificationHandler<TNotification>
             where TNotification : Pinged
         {
-            public void Handle(TNotification notification) { }
+            public void Handle(TNotification notification) { Console.WriteLine(nameof(ConstrainedPingedHandler<TNotification>)); }
         }
     }
 }
---
+++ AspNetDI/ServiceCollectionExtensions.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using static AspNetDI.MicrosoftDITest;
+
+namespace AspNetDI
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void RegisterNotificationHandlers(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            RegisterNotificationHandlersInternal(services, assemblies);
+        }
+
+        public static void RegisterRequestHandlers(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            RegisterRequestHandlersInternal(services, assemblies);
+        }
+
+        public static void RegisterRequestProcessors(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            RegisterRequestProcessorsInternal(services, assemblies);
+        }
+
+        private static void RegisterNotificationHandlersInternal(IServiceCollection services, IEnumerable<Assembly> assembliesToScan)
+        {
+            var openNotificationHandlerInterfaces = new[]
+            {
+                typeof(INotificationHandler<>),
+            };
+            AddInterfacesAsTransient(openNotificationHandlerInterfaces, services, assembliesToScan, true);
+        }
+
+        private static void RegisterRequestHandlersInternal(IServiceCollection services, IEnumerable<Assembly> assembliesToScan)
+        {
+            var openRequestInterfaces = new Type[]
+            {
+                //typeof(IRequestHandler<,>),
+                //typeof(IRequestHandler<>)
+            };
+            AddInterfacesAsTransient(openRequestInterfaces, services, assembliesToScan, false);
+        }
+
+        private static void RegisterRequestProcessorsInternal(IServiceCollection services, IEnumerable<Assembly> assembliesToScan)
+        {
+            var multiOpenInterfaces = new Type[]
+            {
+                //typeof(IRequestPreProcessor<>),
+                //typeof(IRequestPostProcessor<,>)
+            };
+
+            foreach (var multiOpenInterface in multiOpenInterfaces)
+            {
+                var concretions = new List<Type>();
+
+                foreach (var type in assembliesToScan.SelectMany(a => a.ExportedTypes))
+                {
+                    IEnumerable<Type> interfaceTypes = type.FindInterfacesThatClose(multiOpenInterface).ToArray();
+                    if (!interfaceTypes.Any()) continue;
+
+                    if (type.IsConcrete())
+                    {
+                        concretions.Add(type);
+                    }
+                }
+
+                concretions
+                    .Where(HasNoConstraints)
+                    .ToList()
+                    .ForEach(c => services.AddTransient(multiOpenInterface, c));
+            }
+        }
+
+        private static void AddInterfacesAsTransient(Type[] openRequestInterfaces,
+            IServiceCollection services,
+            IEnumerable<Assembly> assembliesToScan,
+            bool addIfAlreadyExists)
+        {
+            foreach (var openInterface in openRequestInterfaces)
+            {
+                var concretions = new List<Type>();
+                var interfaces = new List<Type>();
+
+                foreach (var type in assembliesToScan.SelectMany(a => a.ExportedTypes))
+                {
+                    IEnumerable<Type> interfaceTypes = type.FindInterfacesThatClose(openInterface).ToArray();
+                    if (!interfaceTypes.Any()) continue;
+
+                    if (type.IsConcrete())
+                    {
+                        concretions.Add(type);
+                    }
+
+                    foreach (Type interfaceType in interfaceTypes)
+                    {
+                        interfaces.Fill(interfaceType);
+                    }
+                }
+
+                foreach (var @interface in interfaces)
+                {
+                    var matches = concretions
+                        .Where(HasNoConstraints)
+                        .Where(t => t.CanBeCastTo(@interface))
+                        .ToList();
+                    if (addIfAlreadyExists)
+                    {
+                        matches
+                            .ForEach(match => services.AddTransient(@interface, match));
+                    }
+                    else
+                    {
+                        matches
+                            .ForEach(match => services.TryAddTransient(@interface, match));
+                    }
+
+                    if (!@interface.IsOpenGeneric())
+                    {
+                        AddConcretionsThatCouldBeClosed(@interface, concretions, services);
+                    }
+                }
+            }
+        }
+
+        private static void AddConcretionsThatCouldBeClosed(Type @interface, List<Type> concretions, IServiceCollection services)
+        {
+            foreach (var type in concretions
+                .Where(x => x.IsOpenGeneric() && x.CouldCloseTo(@interface)))
+            {
+                try
+                {
+                    services.TryAddTransient(@interface, type.MakeGenericType(@interface.GenericTypeArguments));
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        private static bool CouldCloseTo(this Type openConcretion, Type closedInterface)
+        {
+            var openInterface = closedInterface.GetGenericTypeDefinition();
+            var arguments = closedInterface.GenericTypeArguments;
+
+            var concreteArguments = openConcretion.GenericTypeArguments;
+            return arguments.Length == concreteArguments.Length && openConcretion.CanBeCastTo(openInterface);
+        }
+
+        private static bool CanBeCastTo(this Type pluggedType, Type pluginType)
+        {
+            if (pluggedType == null) return false;
+
+            if (pluggedType == pluginType) return true;
+
+            return pluginType.GetTypeInfo().IsAssignableFrom(pluggedType.GetTypeInfo());
+        }
+
+        private static bool IsOpenGeneric(this Type type)
+        {
+            return type.GetTypeInfo().IsGenericTypeDefinition || type.GetTypeInfo().ContainsGenericParameters;
+        }
+
+        private static IEnumerable<Type> FindInterfacesThatClose(this Type pluggedType, Type templateType)
+        {
+            if (!pluggedType.IsConcrete()) yield break;
+
+            if (templateType.GetTypeInfo().IsInterface)
+            {
+                foreach (
+                    var interfaceType in
+                        pluggedType.GetTypeInfo().ImplementedInterfaces
+                            .Where(type => type.GetTypeInfo().IsGenericType && (type.GetGenericTypeDefinition() == templateType)))
+                {
+                    yield return interfaceType;
+                }
+            }
+            else if (pluggedType.GetTypeInfo().BaseType.GetTypeInfo().IsGenericType &&
+                     (pluggedType.GetTypeInfo().BaseType.GetGenericTypeDefinition() == templateType))
+            {
+                yield return pluggedType.GetTypeInfo().BaseType;
+            }
+
+            if (pluggedType == typeof(object)) yield break;
+            if (pluggedType.GetTypeInfo().BaseType == typeof(object)) yield break;
+
+            foreach (var interfaceType in FindInterfacesThatClose(pluggedType.GetTypeInfo().BaseType, templateType))
+            {
+                yield return interfaceType;
+            }
+        }
+
+        private static bool IsConcrete(this Type type)
+        {
+            return !type.GetTypeInfo().IsAbstract && !type.GetTypeInfo().IsInterface;
+        }
+
+        private static void Fill<T>(this IList<T> list, T value)
+        {
+            if (list.Contains(value)) return;
+            list.Add(value);
+        }
+
+        private static bool HasNoConstraints(Type type)
+        {
+            var ret = !(type.ContainsGenericParameters && type.GetGenericArguments().Any(p => p.GetGenericParameterConstraints().Any()));
+            return ret;
+        }
+    }
+}
